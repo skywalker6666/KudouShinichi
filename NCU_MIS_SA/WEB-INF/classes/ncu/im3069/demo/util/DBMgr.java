c5c3677e8428ee8916189a00a08eea367331c1cf
fire1 <fire1@MSI>
2019-12-13 00:00:12 +0800

    你們好

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/util/DBMgr.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/util/DBMgr.java
@@ -28,7 +28,7 @@ public class DBMgr {
     static final String USER = "root";
 
     /** PASS常數，所有使用之資料庫使用者密碼 */
-    static final String PASS = "yoyo922922";
+    static final String PASS = "shadow13579";
 
     /** 靜態指定所要使用之Class名稱 **/
     static {
