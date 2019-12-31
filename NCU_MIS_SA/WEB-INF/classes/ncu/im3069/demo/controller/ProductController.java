7a0574ddc840a274be23b0e09dc56a8277dce59e
NCUwalker <alan40406@gmail.com>
2019-12-31 13:23:38 +0800

    edit product controller

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/ProductController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/ProductController.java
@@ -6,6 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.json.*;
 
+import ncu.im3069.demo.app.Product;
 import ncu.im3069.demo.app.ProductHelper;
 import ncu.im3069.tools.JsonReader;
 
@@ -56,9 +57,50 @@ public class ProductController extends HttpServlet {
         jsr.response(resp, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+	        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+	        JsonReader jsr = new JsonReader(request);
+	        JSONObject jso = jsr.getObject();
+	        
+	        /** 取出經解析到JSONObject之Request參數 */
+	        int idtbl_product = jso.getInt("idtbl_product");
+	        String product_name = jso.getString("product_name");
+	        int price = jso.getInt("price");
+	        int inventory = jso.getInt("inventory");
+	        int shopID = jso.getInt("shopID");
+	        int is_deleted = jso.getInt("is_deleted");
+	        String image = jso.getString("image");
+	        String product_info = jso.getString("product_info");
+	 
+
+	        
+	        /** 建立一個新的商品物件 */
+	        Product p = new Product(idtbl_product, product_name, price, inventory, shopID, is_deleted, image,  product_info);
+	        
+	        /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+	        if(image.isEmpty() || product_info.isEmpty() || product_name.isEmpty()) {
+	            /** 以字串組出JSON格式之資料 */
+	            String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+	            /** 透過JsonReader物件回傳到前端（以字串方式） */
+	            jsr.response(resp, response);
+	        }
+	        /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+	        else {
+	            /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
+	            JSONObject data = ph.(p);
+	            
+	            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+	            JSONObject resp = new JSONObject();
+	            resp.put("status", "200");
+	            resp.put("message", "成功! 增加商品資料...");
+	            resp.put("response", data);
+	            
+	            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+	            jsr.response(resp, response);
+	        }
+	        
+	    }
+
 
 }
