cf2a687a85956119df7f0b12543d348277face3c
AlightQQ <greenfish0928@gmail.com>
2020-01-03 18:26:16 +0800

    6.26

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/util/DBMgr.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/util/DBMgr.java
@@ -22,7 +22,7 @@ public class DBMgr {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     /** DB_URL常數，指定資料庫所在之IP或網域、Port號碼與指定所要使用資料庫 */
-    static final String DB_URL = "jdbc:mysql://192.168.0.100:3306/missa?allowPublicKeyRetrieval=true&useSSL=false";
+    static final String DB_URL = "jdbc:mysql://192.168.0.102:3306/missa?allowPublicKeyRetrieval=true&useSSL=false";
 
     /** USER常數，所要使用之資料庫使用者帳號 */
     static final String USER = "general";
